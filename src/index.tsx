4122b2257cf1ae95fcd590f3ca9e36d78cd5d715
Stephane Candelas <stephanecandelas@gmail.com>
2024-02-29 23:42:40 +0000

    add Navbar component & organise folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,16 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import React, { FC } from 'react'
-import { useState } from 'react'
-import './scss/main.scss'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Collapse from 'react-bootstrap/Collapse'
+import ThemeProvider from './context/themeContext'
+import App from './App'
 
-import Button from 'react-bootstrap/Button'
-import Stack from 'react-bootstrap/Stack'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
-const App: React.FC = () => {
-    return (
-        <Navbar expand="lg" bg="light" data-bs-theme="light">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="main-navbar-nav" />
-                <Navbar.Collapse id="main-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#DatePicker">DatePicker</Nav.Link>
-                        <Nav.Link href="#DateRange">DateRange</Nav.Link>
-                        <Nav.Link href="#AppleCalendar">AppleCalendar</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+import './styles/main.scss'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
-root.render(<App />)
+root.render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+)
