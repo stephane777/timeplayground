24c58a3b12bca76486ad21d52dec230b724f0cc2
Stephane Candelas <stephanecandelas@gmail.com>
2024-03-01 21:22:58 +0000

    add extensions to resolve

--- webpack.common.js
+++ webpack.common.js
@@ -1,49 +1,49 @@
-const path = require('path')
-const HtmlWEbpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWEbpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'ts-loader',
-                },
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                mimetype: 'image/svg+xml',
-                scheme: 'data',
-                type: 'asset/resource',
-                generator: {
-                    filename: 'icons/[name][hash].svg',
-                },
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [new HtmlWEbpackPlugin({ template: './src/main.html' })],
-    // optimization: {
-    //     splitChunks: {
-    //         chunks: 'all',
-    //     },
-    // },
-    output: {
-        filename: '[name].[contenthash].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-}
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        mimetype: 'image/svg+xml',
+        scheme: 'data',
+        type: 'asset/resource',
+        generator: {
+          filename: 'icons/[name][hash].svg',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.scss'],
+  },
+  plugins: [new HtmlWEbpackPlugin({ template: './src/main.html' })],
+  // optimization: {
+  //     splitChunks: {
+  //         chunks: 'all',
+  //     },
+  // },
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
