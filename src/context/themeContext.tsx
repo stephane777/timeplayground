05cae851408f859673a5ecaf934b03c2b2138b99
Stephane Candelas <stephanecandelas@gmail.com>
2024-03-01 21:05:31 +0000

    update theme context

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -1,49 +1,39 @@
-import React, {
-    createContext,
-    useMemo,
-    useState,
-    useContext,
-    ReactNode,
-    FC,
-} from 'react'
+import React, { createContext, useMemo, useState, useContext, ReactNode, FC } from 'react';
 
-type Theme = 'light' | 'dark'
+export type Theme = 'light' | 'dark';
 
 interface Props {
-    children?: ReactNode
+  children?: ReactNode;
 }
 
-const ThemeContext = createContext({} as ReturnType<typeof usePassedDownValues>)
+const ThemeContext = createContext({} as ReturnType<typeof usePassedDownValues>);
 
 /** Below is for the Chrome React dev Tools extension
  * to display the name of the context instead
  * of "Context.provider"
  **/
-ThemeContext.displayName = 'ThemeContext'
+ThemeContext.displayName = 'ThemeContext';
 
 export const useTheme = () => {
-    const context = useContext(ThemeContext)
-    if (context === undefined) {
-        throw new Error('useTheme must be used within a ValueProvider')
-    }
-}
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ValueProvider');
+  }
+  return context;
+};
 
 const usePassedDownValues = () => {
-    const [theme, setTheme] = useState<Theme>('light')
+  const [theme, setTheme] = useState<Theme>('light');
 
-    //The state should be memoized to maintain the referential equality/ same location in memory. If not
-    // every time this context is called a new location in memory will be created for the values.
-    const value = useMemo(() => {
-        return { theme, setTheme }
-    }, [theme, setTheme])
-    return value
-}
+  //The state should be memoized to maintain the referential equality/ same location in memory. If not
+  // every time this context is called a new location in memory will be created for the values.
+  const value = useMemo(() => {
+    return { theme, setTheme };
+  }, [theme, setTheme]);
+  return value;
+};
 
 const ThemeProvider: FC<Props> = ({ children }) => {
-    return (
-        <ThemeContext.Provider value={usePassedDownValues()}>
-            {children}{' '}
-        </ThemeContext.Provider>
-    )
-}
-export default ThemeProvider
+  return <ThemeContext.Provider value={usePassedDownValues()}>{children} </ThemeContext.Provider>;
+};
+export default ThemeProvider;
