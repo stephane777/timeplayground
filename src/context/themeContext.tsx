fcd57eecaea854b496650c6c7d8b2e9d9c4ee88d
Stephane Candelas <stephanecandelas@gmail.com>
2024-03-01 00:02:05 +0000

    fix useTheme

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -13,7 +13,7 @@ interface Props {
     children?: ReactNode
 }
 
-const ThemeContext = createContext({} as ReturnType<typeof useTheme>)
+const ThemeContext = createContext({} as ReturnType<typeof usePassedDownValues>)
 
 /** Below is for the Chrome React dev Tools extension
  * to display the name of the context instead
@@ -22,8 +22,17 @@ const ThemeContext = createContext({} as ReturnType<typeof useTheme>)
 ThemeContext.displayName = 'ThemeContext'
 
 export const useTheme = () => {
+    const context = useContext(ThemeContext)
+    if (context === undefined) {
+        throw new Error('useTheme must be used within a ValueProvider')
+    }
+}
+
+const usePassedDownValues = () => {
     const [theme, setTheme] = useState<Theme>('light')
 
+    //The state should be memoized to maintain the referential equality/ same location in memory. If not
+    // every time this context is called a new location in memory will be created for the values.
     const value = useMemo(() => {
         return { theme, setTheme }
     }, [theme, setTheme])
@@ -32,7 +41,7 @@ export const useTheme = () => {
 
 const ThemeProvider: FC<Props> = ({ children }) => {
     return (
-        <ThemeContext.Provider value={useTheme()}>
+        <ThemeContext.Provider value={usePassedDownValues()}>
             {children}{' '}
         </ThemeContext.Provider>
     )
