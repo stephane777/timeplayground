2bb668846920276265d4361a001a303d2142f7fb
Stephane Candelas <stephanecandelas@gmail.com>
2024-03-06 23:10:55 +0000

    add a wrapper layout to DatePicker

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,9 +3,14 @@ import MainNavbar from '../MainNavbar/MainNavbar';
 import { useTheme } from '../../context/themeContext';
 import styles from './App.module.scss';
 import classNames from 'classnames';
+import DatePicker from '../DatePicker/DatePicker';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const App: FC = () => {
   const { theme } = useTheme();
+  const [isFocused, setFocused] = React.useState(false);
 
   const classes = classNames(styles[`theme--${theme}`], styles[`theme`]);
 
@@ -14,7 +19,13 @@ const App: FC = () => {
       <header>
         <MainNavbar />
       </header>
-      <main></main>
+      <main>
+        <Container fluid="sm">
+          <div className={styles[`container__datePicker`]}>
+            <DatePicker onFocusChange={(focused) => setFocused(focused)} focused={isFocused} />
+          </div>
+        </Container>
+      </main>
       <footer></footer>
     </div>
   );
