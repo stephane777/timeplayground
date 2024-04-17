ada0f5be377c7fc95d8d63079b5e5089242c5ad7
Stephane Candelas <stephanecandelas@gmail.com>
2024-04-17 23:59:36 +0100

    feat: add highlight today

--- src/components/RangeCard/RangeCard.tsx
+++ src/components/RangeCard/RangeCard.tsx
@@ -204,10 +204,21 @@ const RangeCard: React.ForwardRefExoticComponent<
 
     return monthArr.map((day, i) => {
       const day_time = moment(timeFirstDay).add(day, 'day').format('YYYY/MM/DD');
+      const now = moment().format('YYYY/MM/DD');
       const isStartTime = moment(day_time).isSame(startTime, 'day');
       const isEndTime = moment(day_time).isSame(endTime, 'day');
       const isBetween =
         moment(day_time).isAfter(startTime, 'day') && moment(day_time).isBefore(endTime, 'day');
+
+      const isTodayAndNotSelected =
+        moment(day_time).isSame(now, 'day') && !isStartTime && !isEndTime && !isBetween;
+
+      // if ( moment(day_time).isSame(now, 'day')){
+      //     console.group('Today')
+      //     console.log('moment(day_time).isSame(now, \'day\'): ', moment(day_time).isSame(now, 'day'))
+      //     console.log('isStartTime: ', isStartTime)
+      //     console.groupEnd()
+      // }
 
       const daysInMonth_classes = classNames(
         styles[`rangeCard__day`],
@@ -217,6 +228,7 @@ const RangeCard: React.ForwardRefExoticComponent<
             'rangeCard__day--endTime': isEndTime,
             'rangeCard__day--startTime': isStartTime,
             [`rangeCard__day--between-${theme}`]: isBetween,
+            'rangeCard__day--today': isTodayAndNotSelected,
           })
         ]
       );
