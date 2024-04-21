208fc8b9621a7f0e1ec3ba2ed4ef966fc083fabf
Stephane Candelas <stephanecandelas@gmail.com>
2024-04-21 23:50:16 +0100

    feat: update page for background

--- src/pages/BerlinClockPage.tsx
+++ src/pages/BerlinClockPage.tsx
@@ -1,14 +1,18 @@
 import React, { FC } from 'react';
 import BerlinClock from '../components/BerlinClock/BerlinClock';
 import styles from '../components/App/App.module.scss';
+import styles2 from '../components/BerlinClock/BerlinClock.module.scss';
+import Container from 'react-bootstrap/Container';
 
 const BerlinClockPage: FC = () => {
   return (
-    <section>
-      <h1 className="">Berlin Clock</h1>
-      <div className={styles[`container__datePicker`]}>
-        <BerlinClock time={'12:56:01'} />
-      </div>
+    <section className={styles2['berlinClock__container']}>
+      <Container fluid="sm" className="pt-5">
+        <h1 className="">Berlin Clock</h1>
+        <div className={styles[`container__datePicker`]}>
+          <BerlinClock time={'12:56:01'} />
+        </div>
+      </Container>
     </section>
   );
 };
