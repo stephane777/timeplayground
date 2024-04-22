39ec1cdde4cc2ef6e12e3fa6514d475041d160b7
Stephane Candelas <stephanecandelas@gmail.com>
2024-04-22 23:18:35 +0100

    feat: add clock functionality

--- src/components/BerlinClock/BerlinClock.tsx
+++ src/components/BerlinClock/BerlinClock.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 import moment from 'moment';
 import classNames from 'classnames';
 import styles from './BerlinClock.module.scss';
@@ -8,98 +8,103 @@ interface BerlinClockProps {
   time: string; // 15:45:06
 }
 
-// function berlinClock(time) {
-//   let [h, m, s] = time.match(/\d+/g).map(x=>+x)
-//   return [
-//     s % 2 ? 'O' : 'Y',
-//     'R'.repeat(h / 5 | 0).padEnd(4, 'O'),
-//     'R'.repeat(h % 5).padEnd(4, 'O'),
-//     'Y'.repeat(m / 5 | 0).replace(/(..)./g, '$1R').padEnd(11, 'O'),
-//     'Y'.repeat(m % 5).padEnd(4, 'O')
-//   ].join('\n')
-// }
+interface BerlinClockState {
+  second: 'Y' | 'O';
+  fiveHoursCell: Array<'R' | 'O'>;
+  hours: Array<'R' | 'O'>;
+  fiveMinutesCell: Array<'R' | 'Y' | 'O'>;
+  minutes: Array<'Y' | 'O'>;
+}
+
+function convertTime(time: string): BerlinClockState {
+  let [h, m, s] = time.split(':').map(Number);
+
+  return {
+    second: s % 2 ? 'O' : 'Y',
+    fiveHoursCell: Array.from({ length: 4 })
+      .fill('O')
+      .map((_, index) => {
+        return h >= (index + 1) * 5 ? 'R' : 'O';
+      }),
+    hours: Array.from({ length: 4 })
+      .fill('O')
+      .map((_, index) => {
+        return h % 5 >= index + 1 ? 'R' : 'O';
+      }),
+    fiveMinutesCell: Array.from({ length: 11 })
+      .fill('O')
+      .map((_, index) => {
+        const total = (index + 1) * 5;
+        return m >= (index + 1) * 5 ? (total % 15 === 0 ? 'R' : 'Y') : 'O';
+      }),
+    minutes: Array.from({ length: 4 })
+      .fill('O')
+      .map((_, index) => {
+        return m % 5 >= index + 1 ? 'Y' : 'O';
+      }),
+  };
+}
 
 const BerlinClock: FC<BerlinClockProps> = ({ time }) => {
-  const repeatChar = (
-    char: 'R' | 'Y',
-    time: number,
-    operator: 'divide' | 'modulo',
-    unit: number,
-    perRow: number
-  ) => {
-    return (
-      char.repeat(Math.floor(operator === 'divide' ? time / unit : time % unit)) +
-      'O'.repeat(perRow - Math.floor(operator === 'divide' ? time / unit : time % unit))
-    );
-  };
+  const [state, setState] = useState<BerlinClockState>(() => convertTime(time));
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newTime = convertTime(moment().format('HH:mm:ss'));
 
-  const berlinClock = (time: string) => {
-    const [h, m, s] = time.split(':').map(Number);
-    const firstRow = s % 2 === 0 ? 'Y' : 'O';
-    const secondRow = repeatChar('R', h, 'divide', 5, 4);
-    const thirdRow = repeatChar('R', h, 'modulo', 5, 4);
-    const forthRow_temp = repeatChar('Y', m, 'divide', 5, 11);
-    const forthRow = Array.from(forthRow_temp)
-      .map((light, i) => ([3, 6, 9].includes(i + 1) && light === 'Y' ? 'R' : light))
-      .join('');
+      setState(newTime);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
-    // const fifthRow = 'Y'.repeat(Math.floor(minutes % 5)) + 'O'.repeat(4 - Math.floor(minutes % 5));
-    const fifthRow = repeatChar('Y', m, 'modulo', 5, 4);
-    return {
-      h,
-      m,
-      s,
-      firstRow,
-      secondRow,
-      thirdRow,
-      forthRow,
-      fifthRow,
-      result: `${firstRow}\n${secondRow}\n${thirdRow}\n${forthRow}\n${fifthRow}`,
-    };
-  };
-  const result = berlinClock(time);
-  console.log('result:', result);
+  const second_classes = classNames(
+    styles['berlinClock__second'],
+    styles[
+      cx({
+        [`berlinClock__second--${state.second}`]: true,
+      })
+    ]
+  );
 
   return (
     <div className="d-flex flex-column align-items-center justify-content-center mt-5">
-      {/* <pre>
-        <code>{JSON.stringify(result)}</code>
-      </pre> */}
       {/** SECOND*/}
-      <div className={styles['berlinClock__second']}></div>
+      <div className={second_classes}></div>
       <div className={styles['berlinClock__join']}></div>
       {/** hours cell 5h*/}
       <div className={styles['berlinClock__row-cells-container']}>
-        <BuildCell numberOfCells={4} />
+        <BuildCell cells={state.fiveHoursCell} />
       </div>
       {/** hours cell 1h*/}
       <div className={styles['berlinClock__join']}></div>
       <div className={styles['berlinClock__row-cells-container']}>
-        <BuildCell numberOfCells={4} />
+        <BuildCell cells={state.hours} />
       </div>
       {/** minutes cell 5min*/}
       <div className={styles['berlinClock__join']}></div>
       <div className={styles['berlinClock__row-cells-container']}>
-        <BuildCell numberOfCells={11} />
+        <BuildCell cells={state.fiveMinutesCell} />
       </div>
       {/** minutes cell 1min*/}
       <div className={styles['berlinClock__join']}></div>
       <div className={styles['berlinClock__row-cells-container']}>
-        <BuildCell numberOfCells={4} />
+        <BuildCell cells={state.minutes} />
       </div>
+      <span className={styles['berlinClock__time']}>{moment().format('HH:mm:ss')}</span>
     </div>
   );
 };
 
-const BuildCell = ({ numberOfCells }: { numberOfCells: number }) => {
-  return Array.from({ length: numberOfCells })
-    .fill(1)
-    .map((cell) => {
-      return (
-        <>
-          <div className={styles['berlinClock__row-cell']}></div>
-        </>
-      );
-    });
+const BuildCell: FC<{ cells: Array<'Y' | 'R' | 'O'> }> = ({ cells }) => {
+  return cells.map((cell, index) => {
+    const cell_classNames = classNames(
+      styles['berlinClock__row-cell'],
+      styles[
+        cx({
+          [`berlinClock__row-cell--${cell}`]: true,
+        })
+      ]
+    );
+    return <div key={index} className={cell_classNames}></div>;
+  });
 };
 export default BerlinClock;
--- src/pages/BerlinClockPage.tsx
+++ src/pages/BerlinClockPage.tsx
@@ -3,14 +3,18 @@ import BerlinClock from '../components/BerlinClock/BerlinClock';
 import styles from '../components/App/App.module.scss';
 import styles2 from '../components/BerlinClock/BerlinClock.module.scss';
 import Container from 'react-bootstrap/Container';
+import moment from 'moment';
 
 const BerlinClockPage: FC = () => {
+  const currentTime = moment().format('HH:mm:ss');
+
+  // console.log('currentTime: ', currentTime);
   return (
     <section className={styles2['berlinClock__container']}>
       <Container fluid="sm" className="pt-5">
         <h1 className="">Berlin Clock</h1>
         <div className={styles[`container__datePicker`]}>
-          <BerlinClock time={'12:56:01'} />
+          <BerlinClock time={'17:28:04'} />
         </div>
       </Container>
     </section>
