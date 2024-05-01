84833de82472cdfb9601c1e099997c540adf3213
Stephane Candelas <stephanecandelas@gmail.com>
2024-05-01 20:55:42 +0100

    chore: add .husky & update gitignore

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,32 +1,32 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const autoprefixer = require('autoprefixer')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = merge(common, {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [autoprefixer],
-                            },
-                        },
-                    },
-                    'postcss-loader',
-                    'sass-loader',
-                ],
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer],
+              },
             },
+          },
+          'postcss-loader',
+          'sass-loader',
         ],
-    },
-    plugins: [new MiniCssExtractPlugin({ filename: '[name].css' })],
-})
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: '[name].css' })],
+});
