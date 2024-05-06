48e3e6596b4602ce4eaf147fc2a513e7f50b9d2c
Stephane Candelas <stephanecandelas@gmail.com>
2024-05-06 14:13:32 +0100

    feat: add Layout test

---
+++ src/components/Layout/Layout.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+
+import { Layout } from './Layout';
+
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+describe('Layout', () => {
+  test('Layout test render', () => {
+    const FakeComponent = () => <div>fake text</div>;
+
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<FakeComponent />} />
+            ff
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Toggle navigation' }));
+    expect(screen.getByRole('main')).toBeInTheDocument();
+    expect(screen.getByRole('contentinfo')).toBeInTheDocument();
+    expect(screen.getAllByRole('link')).toHaveLength(5);
+
+    // test Outlet
+    expect(screen.queryByText('fake text')).toBeInTheDocument();
+  });
+});
