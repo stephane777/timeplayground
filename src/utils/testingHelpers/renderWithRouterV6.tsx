6c1ea3c28acb423d40a9d2473e108f466ef727e2
Stephane Candelas <stephanecandelas@gmail.com>
2024-05-08 23:20:30 +0100

    feat: add renderWithRouterV6 test helper

---
+++ src/utils/testingHelpers/renderWithRouterV6.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+// import { userEvent } from '@testing-library/user-event';
+import { RouterProvider, createMemoryRouter, RouteObject } from 'react-router-dom';
+import { isValidElement, ReactNode } from 'react';
+
+export function renderWithRouterV6(
+  children: ReactNode | Pick<RouteObject, 'element' | 'path'>,
+  routes: RouteObject[] = []
+) {
+  const options = isValidElement(children)
+    ? { element: children, path: '/' }
+    : (children as Pick<RouteObject, 'element' | 'path'>);
+
+  const router = createMemoryRouter([{ ...options }, ...routes], {
+    initialEntries: [options.path as string],
+    initialIndex: 1,
+  });
+
+  return render(<RouterProvider router={router} />);
+}
